6b742b66ad20938d9e33ebff24d748452d7ab05b
Austin Little <Rapportus@gmail.com>
2023-07-27 16:27:39 -0500

    Adding poke library sandboxes

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,10 +1,14 @@
 import requests
 import json
+import pokebase as pb
+import asyncio
+import aiopoke
+
+SANDBOX_ADDRESS = "localhost:9080"
 
 
-# REST API Query
 class PokeAPI:
-    server_url = "http://localhost:9080/api/v2"
+    server_url = f"http://{SANDBOX_ADDRESS}/api/v2"
 
     def __init__(self):
         pass
@@ -16,7 +20,47 @@ class PokeAPI:
         return response.json()
 
 
-api = PokeAPI()
+def manual_rest_query():
+    """
+    Manually query the REST API locally
+    """
+    api = PokeAPI()
+    r = api.get_pokemon("bulbasaur")
+    print(json.dumps(r, indent=2))
 
-r = api.get_pokemon("bulbasaur")
-print(json.dumps(r, indent=2))
+
+def pokebase_query():
+    """
+    Query using pokebase
+    Note this uses http://pokeapi.co/api/v2
+    """
+
+    bulb = pb.APIResource("pokemon", "bulbasaur")
+    print(bulb)
+
+
+async def aio_pokeapi_query():
+    """
+    Query using asyncio
+    Note this uses https://pokeapi.co/api/v2
+    """
+    client = aiopoke.AiopokeClient()
+    bulb = await client.get_pokemon("bulbasaur")
+    print(bulb.name)
+    ability = await bulb.abilities[0].ability.fetch()
+    print(ability.name)
+    await client.close()
+
+
+def aio_main():
+    asyncio.run(aio_pokeapi_query())
+
+
+def main():
+    # manual_rest_query()
+    pokebase_query()
+
+
+if __name__ == "__main__":
+    # main()
+    aio_main()
